ab48cf8acad83213052aae5838192014180a243d
Jack Maloney <jackpmaloney1@gmail.com>
2024-04-29 21:03:16 +0100

    Baseline front page complete

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -26,14 +26,14 @@
                 My <span class="text-violet-400"> favourite tech</span> includes Ansible, TailwindCSS, some other crap.
             </p>
             <a href="#" target="_blank"
-            class="blueShadow mx-auto lg:mr-auto text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950">
+            class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950">
                 <div class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"/>
                 <h4 class="relative z-9">Get in touch &rarr;</h4>
             </a>
         </div>
-        <div class="relative shadow-2xl grid place-items-center">
+        <!-- <div class="relative shadow-2xl grid place-items-center"> -->
             <!-- <img src={"images/profile.png"} alt="Jack circa 2013" class="object-cover z-[2] max-h-[70vh]"/> -->
-        </div>
+        <!-- </div> -->
     </section>
     <section id="projects" class="py-20 lg:py-32 flex flex-col gap-24">
         <div class="flex flex-col gap-2 text-center">
