4d7f78209c1407e1d4df80cbf1c727d8cdedeb7e
Jack Maloney <jackpmaloney1@gmail.com>
2024-07-08 22:29:12 +0100

    Change config for kit to azure

--- svelte.config.js
+++ svelte.config.js
@@ -3,15 +3,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		})
+    adapter: azure()
   }
 };
 export default config;
