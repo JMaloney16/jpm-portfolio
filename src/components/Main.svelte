abe8472057ea870d3fcd03ce49a30cb6b19a6a07
Jack Maloney <jackpmaloney1@gmail.com>
2024-08-09 22:38:48 +0100

    Centre title

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -16,8 +16,8 @@
 </script>
 
 <main class="flex flex-col flex-1 p-4">
-    <section id="introPage" class="grid h-screen gap-10 py-8 sm:py-14">
-        <div class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10">
+    <section id="introPage" class="h-screen gap-10 py-8 sm:py-14">
+        <div class="flex flex-col lg:justify-center text-center gap-6 md:gap-8 lg:gap-10">
             <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
                 <span class="poppins text-steel_blue-600">Jack</span> Maloney <br/>
                 Part Time <span class="poppins text-steel_blue-600">Developer</span>
